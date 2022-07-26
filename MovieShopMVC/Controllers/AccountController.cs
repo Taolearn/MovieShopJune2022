e115f7f19016ebf09767305c3c92db59aad03dbb
Reddy57 <abhilash.gaddam@antra.com>
2022-07-26 16:43:48 -0400

    Added Password Hashing

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -1,10 +1,18 @@
 ﻿using ApplicationCore.Models;
+using ApplicationCore.ServiceContracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
         // account/register
         [HttpGet]
         public IActionResult Register()
@@ -16,6 +24,8 @@ namespace MovieShopMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(UserRegisterModel model)
         {
+            // service, hash the password and save in database
+            var user = await _accountService.CreateUser(model);
             return View();
         }
     }
