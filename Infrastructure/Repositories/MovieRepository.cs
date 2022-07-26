8aef715a9f854b4a1a23d4577868a2f85ff3eea5
Abhilash <abhilash.gaddam@antra.com>
2022-07-26 12:33:33 -0400

    small change

--- Infrastructure/Repositories/MovieRepository.cs
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -1,48 +1,45 @@
 ﻿using ApplicationCore.Entities;
 using ApplicationCore.RepositoryContracts;
 using Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Repositories
+namespace Infrastructure.Repositories;
+
+public class MovieRepository : IMovieRepository
 {
-    public class MovieRepository : IMovieRepository
+    private readonly MovieShopDbContext _movieShopDbContext;
+
+    public MovieRepository(MovieShopDbContext dbContext)
     {
-        private readonly MovieShopDbContext _movieShopDbContext;
-        public MovieRepository(MovieShopDbContext dbContext)
-        {
-            _movieShopDbContext = dbContext;
-        }
-              
-        public async Task<Movie> GetById(int id)
-        {
-            // select * from movie where id = 1 join genre, cast, moviegerne, moviecast
-            var movieDetails = await _movieShopDbContext.Movies
-                .Include(m => m.GenresOfMovie).ThenInclude(m => m.Genre)
-                .Include(m => m.CastsOfMovie).ThenInclude(m => m.Cast)
-                .Include(m=> m.Trailers)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            return movieDetails;
-        }
+        _movieShopDbContext = dbContext;
+    }
 
-        public async Task< List<Movie>> GetTop30HighestRevenueMovies()
-        {
-            // call the database with EF Core and get the data
-            // use MovieShopDbContext and Movies DbSet
-            // select top 30 * from Movies order by Revenue
-            // corresponding LINQ Query
+    public async Task<Movie> GetById(int id)
+    {
+        // select * from movie where id = 1 join genre, cast, moviegerne, moviecast
+        var movieDetails = await _movieShopDbContext.Movies
+            .Include(m => m.GenresOfMovie).ThenInclude(m => m.Genre)
+            .Include(m => m.CastsOfMovie).ThenInclude(m => m.Cast)
+            .Include(m => m.Trailers)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        return movieDetails;
+    }
 
-            var movies = await _movieShopDbContext.Movies.OrderByDescending(m => m.Revenue).Take(30).ToListAsync();
-            return movies;
-        }
+    public async Task<List<Movie>> GetTop30HighestRevenueMovies()
+    {
+        // call the database with EF Core and get the data
+        // use MovieShopDbContext and Movies DbSet
+        // select top 30 * from Movies order by Revenue
+        // corresponding LINQ Query
 
-        public Task< List<Movie>> GetTop30RatedMovies()
-        {
-            throw new NotImplementedException();
-        }
+        var movies = await _movieShopDbContext.Movies.OrderByDescending(m => m.Revenue)
+            .Select(m => new Movie { Id = m.Id, Title = m.Title, PosterUrl = m.PosterUrl})
+            .Take(30).ToListAsync();
+        return movies;
+    }
+
+    public Task<List<Movie>> GetTop30RatedMovies()
+    {
+        throw new NotImplementedException();
     }
 }
